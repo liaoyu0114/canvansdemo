5dc298167277689bb9cd40cb4fdd6ad20d57669c
liaoyu <1453473547@qq.com>
2020-07-08 20:03:58 +0800

    initial

---
+++ src/components/Hexagon.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="hexagon">
+    <div class="bar-rotate-right">
+      <div class="bar-rotate-left">
+        <div class="bar-horizontal">
+          <img :src="src" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Hexagon",
+    props: {
+      src: String
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style scoped>
+  .hexagon {
+    display: inline-block;
+    margin: 0 3px;
+  }
+
+  .hexagon .bar-rotate-left {
+    width: 200px;
+    height: 231px;
+    overflow: hidden;
+    transform: rotate(60deg);
+    display: inline-block;
+  }
+
+  .hexagon .bar-rotate-right {
+    width: 200px;
+    height: 231px;
+    overflow: hidden;
+    transform: rotate(60deg);
+    display: inline-block;
+  }
+
+  .hexagon .bar-horizontal {
+    width: 200px;
+    height: 232px;
+    background: #FFFFFF;
+    transform: rotate(-120deg);
+    display: inline-block;
+  }
+
+  .hexagon .bar-horizontal:hover {
+    background: #F0F0F0;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,76 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <hexagon v-for="item in 5 " :key="item"></hexagon>
+    </div>
+    <div style="transform: translate(0px, -56.75px)">
+      <hexagon  v-for="item in 6" :key="item" :src="src"></hexagon>
+    </div>
+    <div  style="transform: translate(0, -113.5px)">
+      <hexagon v-for="item in 5" :key="item"></hexagon>
+    </div>
+    <div style="transform: translate(0px, -170.25px)">
+      <hexagon  v-for="item in 6" :key="item"></hexagon>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Hexagon from '@/components/Hexagon.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Hexagon
+  },
+  data() {
+    return {
+      src: "https://dss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1354268575,1268995723&fm=26&gp=0.jpg"
+    }
   }
 }
 </script>
+
+<style scoped>
+  .home {
+    background: #ffc0cb;
+    width: 100vw;
+    height: 100vh;
+    display: inline-block;
+
+    /*transform: translateX(100px);*/
+  }
+.home div {
+  display: inline-block;
+}
+  .bar-rotate-left {
+    width: 200px;
+    height: 231px;
+    overflow: hidden;
+    transform: rotate(60deg);
+    display: inline-block;
+  }
+
+  .bar-rotate-right {
+    width: 200px;
+    height: 231px;
+    overflow: hidden;
+    transform: rotate(60deg);
+    display: inline-block;
+  }
+
+ .bar-horizontal {
+    width: 200px;
+    height: 232px;
+    background: #FFFFFF;
+    transform: rotate(-120deg);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+.bar-horizontal:hover {
+    background: #F0F0F0;
+  }
+</style>
